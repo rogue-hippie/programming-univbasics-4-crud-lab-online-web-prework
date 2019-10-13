0e57ffe4a2f41784a7832380bfffa4262233bdd8
funny-array-8950 <hodgkins.whitney@gmail.com>
2019-10-13 20:58:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,10 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array(index_number)
+  family_kids = ["Cassidy", "Brice Jr.", "Jordan", "Zoe"]
 end
+
+family_kids(1)
 
 def retrieve_first_element_from_array(array)
   
